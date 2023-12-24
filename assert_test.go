ed89913d2f0aef081d37116ae648b3456f1e64ce
muz-ooh <mauricio.riquelme@oohmedia.com.au>
2023-12-24 11:26:37 +1100

    adding test

--- assert_test.go
+++ assert_test.go
@@ -38,3 +38,85 @@ func TestDecorateDiff(t *testing.T) {
 
 	AssertNoError(t, err)
 }
+
+func TestAssertNil(t *testing.T) {
+	group := NewGroup(t, nil)
+
+	var nilString string
+	var nilInt int
+	var nilFloat float32
+	var nilBool bool
+
+	err := group.
+		Test("should pass nil string", func(t *testing.T) {
+			AssertNil(t, nilString)
+		}).
+		Test("should pass nil int", func(t *testing.T) {
+			AssertNil(t, nilInt)
+		}).
+		Test("should pass nil float", func(t *testing.T) {
+			AssertNil(t, nilFloat)
+		}).
+		Test("should pass nil bool", func(t *testing.T) {
+			AssertNil(t, nilBool)
+		}).
+		Test("should pass nil empty slice", func(t *testing.T) {
+			AssertNil(t, []string{})
+		}).
+		Run()
+
+	AssertNoError(t, err)
+}
+
+func TestAssertEqual(t *testing.T) {
+	testStruct := struct{ name string }{name: "test"}
+
+	group := NewGroup(t, nil)
+
+	err := group.
+		Test("should pass equal strings", func(t *testing.T) {
+			AssertEqual(t, "a", "a")
+		}).
+		Test("should pass equal ints", func(t *testing.T) {
+			AssertEqual(t, 1, 1)
+		}).
+		Test("should pass equal floats", func(t *testing.T) {
+			AssertEqual(t, 1.1, 1.1)
+		}).
+		Test("should pass equal bool", func(t *testing.T) {
+			AssertEqual(t, true, true)
+		}).
+		Test("should pass identical", func(t *testing.T) {
+			AssertEqual(t, testStruct, testStruct)
+		}).
+		Test("should pass same struct signature", func(t *testing.T) {
+			AssertEqual(t, testStruct, struct{ name string }{name: "test"})
+		}).
+		Test("should pass same slice signature", func(t *testing.T) {
+			AssertEqual(t, []string{
+				"a",
+			}, []string{
+				"a",
+			})
+		}).
+		Test("should pass two empty slices", func(t *testing.T) {
+			AssertEqual(t, []string{}, []string{})
+		}).
+		Run()
+
+	AssertNoError(t, err)
+}
+
+func TestAssertTrue(t *testing.T) {
+	group := NewGroup(t, nil)
+	err := group.
+		Test("should pass on literal", func(t *testing.T) {
+			AssertTrue(t, true)
+		}).
+		Test("should pass on string assertion", func(t *testing.T) {
+			AssertTrue(t, strings.Contains("hello", "hello"))
+		}).
+		Run()
+
+	AssertNoError(t, err)
+}
